4234f505033c531925f87718c90e2f4f1b868522
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-21 20:45:33 +0530

    LinkList

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures;
+
+Console.WriteLine("Hello World!");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+//list.InsertAtParticularPosition(2,30);
+list.Display();
+Console.ReadKey();
